96b71fc5f86e50939b8d5baf908e40b9ad48fa24
Alexandre DeFreitas <abd9@njit.edu>
2014-10-13 20:12:17 -0400

    created csv::getFriendlyHeadings method

--- classes/Csv.class.php
+++ classes/Csv.class.php
@@ -3,10 +3,12 @@
 
 interface CsvInterface {
   public static function open( $path );
-  
+  public static function close( $handle );
+  public static function getFriendlyHeadings($filepath, $ignore_first_row, $column);
 }
 
 class Csv {
+
   public static function open($path) {
     return fopen($path, "r");
   }
@@ -14,6 +16,20 @@ class Csv {
   public static function close($handle) {
     fclose( $handle );
   }
+
+  public static function getFriendlyHeadings($filepath, $ignore_first_row, $column) {
+    ini_set('auto_detect_line_endings', TRUE);
+    if( $handle = self::open($filepath) ) {
+      while( ($row = fgetcsv($handle, 1000, ",")) != FALSE ) {
+        if( $ignore_first_row ) {
+          $ignore_first_row = FALSE;
+        } else {
+          $friendlyHeadings[] = $row[$column];
+        } //end if-else
+      } //end while
+    } //end if
+    return $friendlyHeadings;
+  } //end getFriendly Headings
 
 } // end class Csv
 
